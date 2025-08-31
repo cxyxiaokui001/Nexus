33711612b56b6134af3bea7aae44629238000e55
cxyxiaokui <1827525830@qq.com>
2025-08-31 23:29:56 +0800

    feat: 初始化 Nexus 项目
    - 添加基本的项目结构和配置- 实现健康检查接口
    - 设置全局异常处理
    - 添加跨域配置
    - 定义基础响应类和错误码
    - 实现快速构建响应结果的工具类
    - 添加单元测试

---
+++ src/main/java/com/xiaokui/nexus/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.xiaokui.nexus.controller;
+
+import com.xiaokui.nexus.common.BaseResponse;
+import com.xiaokui.nexus.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
