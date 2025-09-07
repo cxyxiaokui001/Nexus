f4d3561cd40019e67dde2fa29dc87906fd291602
cxyxiaokui <1827525830@qq.com>
2025-09-07 17:01:52 +0800

    feat(app): 应用模块 完成基本功能
    - 新增应用添加、更新、删除等接口
    - 新增应用查询请求和响应数据结构
    - 实现应用相关信息的封装和查询功能
    - 添加管理员相关的应用操作接口

--- src/main/java/com/xiaokui/nexus/service/AppService.java
+++ src/main/java/com/xiaokui/nexus/service/AppService.java
@@ -1,7 +1,12 @@
 package com.xiaokui.nexus.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.xiaokui.nexus.model.dto.app.AppQueryRequest;
 import com.xiaokui.nexus.model.entity.App;
+import com.xiaokui.nexus.model.vo.AppVO;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,9 @@ import com.xiaokui.nexus.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    AppVO getAppVO(App app);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
